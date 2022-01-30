44ec042b6ad8f2dd28f02aa0fa3361d6eb034632
Medokins <nikodemszwast@gmail.com>
2022-01-30 22:15:38 +0100

    add games file

--- games.py
+++ games.py
@@ -1,4 +1,4 @@
 import pandas as pd
 from surprise import Reader
 
-games_df = pd.read_csv("Datasets/user.csv")
+games_df = pd.read_csv("Datasets/user.csv") #I'll try to implement games characteristic combined with Collaborative Filtering from user_df
