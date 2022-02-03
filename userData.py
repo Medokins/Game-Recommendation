c15376a020a2a95fc225f96851fa31f5d8e3dea2
Medokins <nikodemszwast@gmail.com>
2022-02-03 18:08:51 +0100

    add user rating column

--- userData.py
+++ userData.py
@@ -1,3 +1,4 @@
+from numpy import place
 import pandas as pd
 from scipy import sparse
 from sklearn.metrics.pairwise import cosine_similarity
@@ -13,16 +14,38 @@ user_df.drop(user_df.columns[4], axis = 1, inplace = True)
 user_df.columns = ["userId", "game", "purchase/play", "timePlayed"]
 user_df["purchase/play"] =  user_df["purchase/play"].map(purchase_dict)
 
-user_favourites = user_df.pivot_table(index=['userId'], columns=['game'], values='timePlayed')
+def create_rating(hours_played):
+    if hours_played <= 1:
+        return 0.0
+    elif hours_played < 10:
+        return 1.0
+    elif hours_played < 25:
+        return 2.0
+    elif hours_played < 50:
+        return 3.0
+    elif hours_played < 150:
+        return 4.0
+    elif hours_played >= 150:
+        return 5.0
+
+def standardize(row):
+    new_row = (row - row.mean()) / (row.max() - row.min())
+    return new_row
+
+user_df["raiting"] = user_df["timePlayed"].apply(create_rating)
+
+
+user_favourites = user_df.pivot_table(index=['userId'], columns=['game'], values='raiting')
+user_favourites = user_favourites.apply(standardize)
 user_favourites.fillna(0, inplace = True)
 
 game_similarity = cosine_similarity(user_favourites.T)
 game_similarity_df = pd.DataFrame(game_similarity, index=user_df["game"].unique(), columns = user_df["game"].unique())
 
 def get_similar_games(game_name, user_raiting):
-    similar_games = game_similarity_df[game_name] * (user_raiting-50)
+    similar_games = game_similarity_df[game_name] * (user_raiting - 1.0)
     similar_games = similar_games.sort_values(ascending=False)
 
     return similar_games
 
-print(get_similar_games("Quake", 100))
+print(get_similar_games("Borderlands 2", 4))
