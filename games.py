d1a5a44c370f3a1c9d3c863209709b14feed75b0
Medokins <nikodemszwast@gmail.com>
2022-02-04 19:09:18 +0100

    add get_same_genre function

--- games.py
+++ games.py
@@ -1,14 +1,7 @@
 import pandas as pd
-from surprise import Reader
 
 games_df = pd.read_csv("Datasets/games.csv") #Plan is to implement games characteristic combined with Collaborative Filtering from user_df
 index_df = pd.DataFrame(games_df["name"])
-
-# print(games_df["name"].head(1)) #using this as id
-# print(games_df["all_reviews"].head(1)) #maby useful
-# print(games_df["popular_tags"].head(1)) #useful
-# print(games_df["game_details"].head(1)) #useful
-# print(games_df["genre"].head(1)) #useful
 
 index_df = index_df.T
 index_df = (index_df.T.reset_index().T.reset_index(drop=True)
@@ -16,5 +9,19 @@ index_df = (index_df.T.reset_index().T.reset_index(drop=True)
 index_df.drop(1, inplace=True)
 
 useful_game_info = games_df[["name", "popular_tags", "game_details", "genre", "all_reviews"]].copy()
+useful_game_info.dropna(inplace = True)
 
-print(useful_game_info.iloc[index_df["Dishonored®: Death of the Outsider™"]]) #example, will be usefull when I combine this wither user_df
+#I need to drop some rows, some games have inccorect data format
+
+def get_same_genre(game_name):
+    game_genre = useful_game_info.iloc[index_df[game_name]]["genre"][0]
+    most_popular_tag = useful_game_info.iloc[index_df[game_name]]["popular_tags"][0].split(",")[0]
+    same_genre = []
+    for game in useful_game_info["name"]:
+        print(game)
+        print(useful_game_info.iloc[index_df[game]])
+        # if useful_game_info.iloc[index_df[game]]["genre"][0] == game_genre or useful_game_info.iloc[index_df[game]]["popular_tags"][0].split(",")[0] == most_popular_tag:
+        #     same_genre.append(game)
+    return same_genre
+
+print(get_same_genre("DOOM"))
