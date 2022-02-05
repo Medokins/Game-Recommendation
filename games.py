68ecd2e65a57464d0cf163a1875d6158685f0c6c
Medokins <nikodemszwast@gmail.com>
2022-02-05 18:58:56 +0100

    fix typo

--- games.py
+++ games.py
@@ -5,7 +5,7 @@ import numpy as np
 useful_game_info = pd.read_csv("Datasets/useful_game_info.csv")
 index_df = pd.read_csv("Datasets/game_indexes.csv")
 
-def get_same_genre(game_name): #I still need to implement better way to compare gender since not all data formating is the same
+def get_same_genre(game_name): #I still need to implement better way to compare genre since not all data formating is the same
 
     game_genre = useful_game_info.iloc[index_df[game_name]]["genre"][0]
     same_genre = []
