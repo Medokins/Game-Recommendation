1cef4cfaa2189bae66f80cdba71384703a2f6bf3
Medokins <nikodemszwast@gmail.com>
2022-02-06 20:43:41 +0100

    oprimize get_same_genre function

--- games.py
+++ games.py
@@ -1,20 +1,22 @@
-from operator import index
 import pandas as pd
 import numpy as np
 
 useful_game_info = pd.read_csv("Datasets/useful_game_info.csv")
 index_df = pd.read_csv("Datasets/game_indexes.csv")
 
-def get_same_genre(game_name): #I still need to implement better way to compare genre since not all data formating is the same
+def get_same_genre(game_name, verbose=False):
 
-    game_genre = useful_game_info.iloc[index_df[game_name]]["genre"][0]
     same_genre = []
+    game_genre = useful_game_info.iloc[index_df[game_name][0]]["genre"].split(",")[0]
 
     for i in range(len(useful_game_info["name"])):
-        if np.str(useful_game_info.iloc[i]["genre"]).split(",")[0] == game_genre:
+        current_genre = useful_game_info.iloc[i]["genre"].split(",")[0]
+        if current_genre == game_genre:
             game = useful_game_info.iloc[i]["name"]
-            print(f"We got a match! {game} is same genre as {game_name}, the genre is {game_genre}")
             same_genre.append(game)
+            if verbose:
+                print(f"We got a match! {game} is same genre as {game_name}, the genre is {current_genre}")
+
     return same_genre
 
-print(get_same_genre("DOOM"))
+print(get_same_genre("The Elder Scrolls®: Legends™"))
