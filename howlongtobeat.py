ae2b7ffe12e8db1e38d1a4f0230dea71d9aa0ad2
Medokins <nikodemszwast@gmail.com>
2022-02-10 16:50:17 +0100

    add training process for average_time_play dataframe

--- howlongtobeat.py
+++ howlongtobeat.py
@@ -1,16 +1,31 @@
 import pandas as pd
+import numpy as np
 
-# I found https://github.com/ckatzorke/howlongtobeat api which i will use to better implement rating based on user play time + avarage play time to beat game
-#average time played vs user time played should do it instead of this ^ (bcs more than 30% of games in my dataset are missing on this page + it has different formating)
+#I found https://github.com/ckatzorke/howlongtobeat api which i wanted to use to better implement rating based on user play time vs avarage play time to beat game
+#average time played in this dataset vs user time played should do it instead of this ^ (bcs more than 30% of games in my dataset are missing on this page + it has different formating)
 
-useful_game_info = pd.read_csv("Datasets/useful_game_info.csv")
+useful_game_info = pd.read_csv("Datasets/user_df_with_ratings.csv")
+main_df = pd.read_csv("Datasets/game_indexes.csv")
+average_time_played = main_df.copy()
 
-average_time_played = useful_game_info["name"]
-average_time_played = average_time_played.T
-average_time_played = (average_time_played.T.reset_index().T.reset_index(drop=True).set_axis([f'{name}' for name in useful_game_info["name"]], axis=1))
-average_time_played.drop(1, inplace=True)
+counter = 100
+num_of_columns = len(main_df.columns)
 
-for column in average_time_played:
-    average_time_played[column] = 1  #put average time played here in future
+for game in main_df.columns[1:5]:
 
-print(average_time_played.head())
+    number_of_players = 0
+    summ_of_hours = 0
+    percent = counter / num_of_columns
+    print(f"Currently on {percent}%")
+    counter += 1
+
+
+    for row in range(len(useful_game_info["game"])):
+        if useful_game_info.iloc[row]["game"] == game:
+            #print(f"Found {game} in row nr {row}")
+            number_of_players += 1
+            summ_of_hours += useful_game_info.iloc[row]["timePlayed"]
+
+    average_time_played[game][0] = summ_of_hours / np.float(number_of_players)
+
+average_time_played.to_csv("Datasets/average_time_played.csv")
