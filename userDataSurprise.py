1e831f99b999c2135a50ed31cad272335ce09ba3
Medokins <nikodemszwast@gmail.com>
2022-02-13 11:15:17 +0100

    add first recommendation function

--- userDataSurprise.py
+++ userDataSurprise.py
@@ -2,6 +2,7 @@ import pandas as pd
 from surprise import Reader, Dataset, SVD
 from surprise.model_selection import cross_validate
 
+games_df = pd.read_csv("Datasets/game_indexes.csv")
 user_df = pd.read_csv("Datasets/user_df_with_ratings.csv")
 user_df.drop("Unnamed: 0", axis = 1, inplace=True)
 user_df.drop("timePlayed", axis = 1, inplace=True)
@@ -16,8 +17,19 @@ reader = Reader(rating_scale=(0.0, 5.0))
 data = Dataset.load_from_df(df[['userID', 'itemID', 'rating']], reader)
 
 algo = SVD()
-cross_validate(algo, data, measures=['RMSE'], cv=10, verbose=True)
+cross_validate(algo, data, measures=['RMSE'], cv=5, verbose=True)
 
-test_RPG = algo.predict("151603712", "The Witcher 3 Wild Hunt").est #User with this number played Skyrim for more than 150h, but didn't play Witcher which is simmillar in my opinion
-test_FPS = algo.predict("151603712", "DOOM").est
-print(f"RPG fan:\nWitcher: {test_RPG}\nDOOM: {test_FPS}")
+# test_RPG = algo.predict("151603712", "The Witcher 3 Wild Hunt").est #User with this number played Skyrim for more than 150h, but didn't play Witcher which is simmillar in my opinion
+# test_FPS = algo.predict("151603712", "DOOM").est
+# print(f"RPG fan:\nWitcher: {test_RPG}\nDOOM: {test_FPS}")
+
+scores = []
+def get_k_best(user_index, num_of_games = 5):
+    for game in games_df.columns[1:]:
+        game_score = algo.predict(user_index, game).est
+        scores.append([game_score, game])
+    
+    scores.sort(reverse = True)
+    return scores[0:num_of_games]
+
+print(get_k_best("151603712"))
