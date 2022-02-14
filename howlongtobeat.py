1bc5937bf17503c506e3d055ea6e7a7a9ce2b279
Medokins <nikodemszwast@gmail.com>
2022-02-14 14:39:42 +0100

    minor improvement

--- howlongtobeat.py
+++ howlongtobeat.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 #I found https://github.com/ckatzorke/howlongtobeat api which i wanted to use to better implement rating based on user play time vs avarage play time to beat game
-#average time played in this dataset vs user time played should do it instead of this ^ (bcs more than 30% of games in my dataset are missing on this page + it has different formating)
+#average time played in this dataset vs user time played should do it instead of this ^ (because more than 30% of games in my dataset are missing on this page + it has different formating)
 
 train = False
 
@@ -29,7 +29,3 @@ if train:
 
         average_time_played[game][0] = summ_of_hours / np.float(number_of_players)
         average_time_played.to_csv(f"Datasets/average_time_play_{counter}.csv")
-
-
-average_time_played = pd.read_csv("Datasets/game_indexes.csv")
-average_time_played.to_csv(f"Datasets/average_time_play_full.csv")
