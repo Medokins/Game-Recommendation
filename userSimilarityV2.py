915902cb5b2842335b6613d2f01503a18642dbcf
Medokins <nikodemszwast@gmail.com>
2022-02-27 12:40:09 +0100

    add working algorithm

--- userSimilarityV2.py
+++ userSimilarityV2.py
@@ -1,96 +1,7 @@
 import pandas as pd
-import numpy as np
-import scipy.stats
 
-overwrite_data = False
-normalize_ratings = False
-create_similarity_matrix = True
-
-if overwrite_data:
-    user_df = pd.read_csv("Datasets/user_df_with_ratings.csv")
-    main_df = pd.read_csv("Datasets/game_indexes.csv")
-
-    user_df.drop("Unnamed: 0", axis = 1, inplace=True)
-    user_df.drop("timePlayed", axis = 1, inplace=True)
-    main_df.drop("Unnamed: 0", axis = 1, inplace=True)
-
-if normalize_ratings:
-    user_df = pd.read_csv("Datasets/user_df_with_ratings.csv")
-    ratings_df = pd.read_csv("Datasets/user_df_with_all_ratings.csv")
-    ratings_df.drop("Unnamed: 0", axis = 1, inplace=True)
-
-def create_rating_vector(userId, verbose = False):
-
-    rating_vector = []
-    current_user = user_df.loc[user_df["userId"] == userId]
-
-    for game in main_df.columns:
-
-        if game in list(current_user["game"]):
-            rating = list(current_user.loc[user_df["game"] == game]["rating"])[0]
-            rating_vector.append(rating)
-
-            if verbose:
-                print(f"he played {game} and gave it a {rating}")
-
-        else:
-            rating_vector.append(np.NaN)
-
-    
-    return rating_vector
-
-if overwrite_data:
-    for user in user_df["userId"].unique():
-        main_df_length = len(main_df)
-        main_df.loc[main_df_length] = create_rating_vector(userId = user)
-        print(main_df_length / len(user_df["userId"].unique()) * 100, "%")
-    main_df.to_csv("Datasets/user_df_with_all_ratings.csv")
-
-if normalize_ratings:
-    ratings_df.drop([0], axis = 0, inplace=True)
-    ratings_df['userId'] = user_df['userId'].unique()
-    ratings_df.set_index('userId', inplace=True)
-
-    def normalize(dataframe):
-        dataframe_mean = dataframe.mean(axis=1)
-        return dataframe.subtract(dataframe_mean, axis = 0)
-
-    ratings_df = normalize(ratings_df)
-    ratings_df.to_csv("Datasets/user_df_with_all_ratings_normalized.csv")
-
-
-#I'm using scipy which has it own normalization method so im leaving normalized ratings for now
-
-if create_similarity_matrix:
-
-    ratings_df = pd.read_csv("Datasets/user_df_with_all_ratings.csv")
-    ratings_df.drop('userId', axis=1, inplace=True)
-    ratings_df.fillna(99, inplace=True)
-
-    def similarity_pearson(x, y):
-        return scipy.stats.pearsonr(x, y)[0]
-
-    users = len(ratings_df)
-    similarity_matrix = []
-
-    for i in range(users):
-
-        for j in range(users):
-            similarity_matrix.append(similarity_pearson(ratings_df.iloc[i,:], ratings_df.iloc[j,:]))    
-
-        if i%1000 == 0:
-            print(f"Currently:{(i*users + j) / (users*users) * 100}%")
-            textfile = open("backup_file.txt", "w")
-            for element in similarity_matrix:
-                textfile.write(str(element) + " ")
-            textfile.close()
-    
-    similarity_matrix = np.array(similarity_matrix)
-
-    similarity_df = pd.DataFrame(data = similarity_matrix.reshape(users, users))
-    similarity_df.to_csv("Datasets/users_similarity_df.csv")
-
-##################################################################################################
+current_player = 100
+treshold = 0.25 #value from -1.0 to 1.0, the higher it is the more selective algorithm will be
 
 similarity_df = pd.read_csv("Datasets/similarity_df.csv")
 similarity_df.drop("Unnamed: 0", axis = 1, inplace=True)
@@ -98,8 +9,66 @@ similarity_df.drop("Unnamed: 0", axis = 1, inplace=True)
 game_indexes_inverted = pd.read_csv("Datasets/game_indexes_inverted.csv")
 game_indexes_inverted.drop("Unnamed: 0", axis = 1, inplace=True)
 
-def neighbours(sim):
-   return [sim.index[i] for i, v in enumerate(sim) if (v>=0.3)]
+ratings_df = pd.read_csv("Datasets/user_df_with_ratings.csv")
+ratings_df.drop("Unnamed: 0", axis = 1, inplace=True)
 
-current_player = 0
+user_indexes = pd.read_csv("Datasets/user_indexes_inverted.csv")
+user_indexes.drop("Unnamed: 0", axis = 1, inplace=True)
+
+###########################################################################
+
+def neighbours(sim):
+   return [sim.index[i] for i, v in enumerate(sim) if (v>=treshold)]
 similar_players = neighbours(similarity_df.iloc[current_player])
+
+if str(current_player) in similar_players:
+    similar_players.remove(str(current_player))
+
+###########################################################################
+
+similar_players_Ids = []
+for player in similar_players:
+    similar_players_Ids.append(user_indexes[player][0])
+
+###########################################################################
+
+def get_current_player_games(index):
+    active_player_id = user_indexes[str(index)][0]
+    active_player = ratings_df.loc[ratings_df["userId"] == active_player_id]
+    active_player = active_player["game"]
+    active_player_games = []
+
+    for row in range(len(active_player)):
+        game = active_player.iloc[row]
+        if game not in active_player_games:
+            active_player_games.append(game)
+
+    return active_player_games
+
+###########################################################################
+
+active_player_games = get_current_player_games(current_player)
+recommended_games = []
+super_recommended = []
+
+for player in similar_players_Ids:
+
+    current_player = ratings_df.loc[ratings_df["userId"] == player]
+    player_recommendation = current_player.loc[current_player['rating'] >= 4]['game']
+
+    for row in range(len(player_recommendation)):
+        game = player_recommendation.iloc[row]
+
+        if game not in active_player_games:
+            if game not in recommended_games:
+                recommended_games.append(game)
+            else:
+                super_recommended.append(game)
+
+if len(super_recommended) == 0:
+    if len(recommended_games) != 0:
+        print(f"Users similar to You liked: {recommended_games}")
+    else:
+        print(f"Woah, You're quite unique, there is noone like You (at current treshold: {treshold})")
+else:
+    print(f"Multiple players similar to You liked {super_recommended}")
