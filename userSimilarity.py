594b0ecab6325d3f8932bf72db0faa6858cc1609
Medokins <nikodemszwast@gmail.com>
2022-02-28 21:47:57 +0100

    rename userSimilarityV2 to userSimilarity

--- userSimilarity.py
+++ userSimilarity.py
@@ -1,37 +1,74 @@
 import pandas as pd
-from surprise import Reader, Dataset, SVD
-from surprise.model_selection import cross_validate
 
-games_df = pd.read_csv("Datasets/game_indexes.csv")
-user_df = pd.read_csv("Datasets/user_df_with_ratings.csv")
-user_df.drop("Unnamed: 0", axis = 1, inplace=True)
-user_df.drop("timePlayed", axis = 1, inplace=True)
+current_player = 100
+treshold = 0.25 #value from -1.0 to 1.0, the higher it is the more selective algorithm will be
 
-rating_dict = {'itemID': list(user_df["game"]),
-                'userID': list(user_df["userId"]),
-                'rating': list(user_df["rating"])}
+similarity_df = pd.read_csv("Datasets/similarity_df.csv")
+similarity_df.drop("Unnamed: 0", axis = 1, inplace=True)
 
-df = pd.DataFrame(rating_dict)
-reader = Reader(rating_scale=(0.0, 5.0))
+game_indexes_inverted = pd.read_csv("Datasets/game_indexes_inverted.csv")
+game_indexes_inverted.drop("Unnamed: 0", axis = 1, inplace=True)
 
-data = Dataset.load_from_df(df[['userID', 'itemID', 'rating']], reader)
+ratings_df = pd.read_csv("Datasets/user_df_with_ratings.csv")
+ratings_df.drop("Unnamed: 0", axis = 1, inplace=True)
 
-algo = SVD()
-cross_validate(algo, data, measures=['RMSE'], cv=5, verbose=True)
+user_indexes = pd.read_csv("Datasets/user_indexes_inverted.csv")
+user_indexes.drop("Unnamed: 0", axis = 1, inplace=True)
 
-# test_RPG = algo.predict("151603712", "The Witcher 3 Wild Hunt").est #User with this number played Skyrim for more than 150h, but didn't play Witcher which is simmillar in my opinion
-# test_FPS = algo.predict("151603712", "DOOM").est
-# test_FPS_nother = algo.predict("221315846", "DOOM").est
-# print(f"RPG fan:\nWitcher: {test_RPG}\nDOOM: {test_FPS}\n\n {test_FPS_nother}")
+###########################################################################
 
-scores = []
-def get_k_best(user_index, num_of_games = 5):
-    for game in games_df.columns[1:]:
-        game_score = algo.predict(user_index, game).est
-        scores.append([game_score, game])
-    
-    scores.sort(reverse = True)
-    return scores[0:num_of_games]
+def neighbours(sim):
+   return [sim.index[i] for i, v in enumerate(sim) if (v>=treshold)]
+similar_players = neighbours(similarity_df.iloc[current_player])
 
-print(get_k_best(user_index = "221315846"))
-print(get_k_best(user_index = "151603712"))
+if str(current_player) in similar_players:
+    similar_players.remove(str(current_player))
+
+###########################################################################
+
+similar_players_Ids = []
+for player in similar_players:
+    similar_players_Ids.append(user_indexes[player][0])
+
+###########################################################################
+
+def get_current_player_games(index):
+    active_player_id = user_indexes[str(index)][0]
+    active_player = ratings_df.loc[ratings_df["userId"] == active_player_id]
+    active_player = active_player["game"]
+    active_player_games = []
+
+    for row in range(len(active_player)):
+        game = active_player.iloc[row]
+        if game not in active_player_games:
+            active_player_games.append(game)
+
+    return active_player_games
+
+###########################################################################
+
+active_player_games = get_current_player_games(current_player)
+recommended_games = []
+super_recommended = []
+
+for player in similar_players_Ids:
+
+    current_player = ratings_df.loc[ratings_df["userId"] == player]
+    player_recommendation = current_player.loc[current_player['rating'] >= 4]['game']
+
+    for row in range(len(player_recommendation)):
+        game = player_recommendation.iloc[row]
+
+        if game not in active_player_games:
+            if game not in recommended_games:
+                recommended_games.append(game)
+            else:
+                super_recommended.append(game)
+
+if len(super_recommended) == 0:
+    if len(recommended_games) != 0:
+        print(f"Users similar to You liked: {recommended_games}")
+    else:
+        print(f"Woah, You're quite unique, there is noone like You (at current treshold: {treshold})")
+else:
+    print(f"Multiple players similar to You liked {super_recommended}")
